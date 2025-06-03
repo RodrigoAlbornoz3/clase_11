7f8bb8e636a6c5c58ab2186b2a191e6f8f2a7751
RodrigoAlbornoz3 <rodrigocris2004@gmail.com>
2025-06-03 15:21:33 -0400

    Update profes.js

--- js/profes.js
+++ js/profes.js
@@ -497,7 +497,7 @@ async function datos(criterio) {
     }
 }
 
-datos("Dueñas Santander, Rodrigo").catch((error) => console.error(error));
+datos("Mendez Olivales, Ivan").catch((error) => console.error(error));
 
 document.querySelectorAll("select")[0].addEventListener("change", (event) => {
     tabla.innerHTML = " ";
@@ -508,7 +508,7 @@ document.querySelectorAll("select")[0].addEventListener("change", (event) => {
     var notas = [];
     var notasPrevias = [];
     datos(event.target.value).catch((error) => console.error(error));
-    if(event.target.value == "Mendez Olivales, Ivan"){
+    if(event.target.value == "Lares Lopez, Lorna"){
         document.querySelector("#primera").classList.add("esconde");
         document.querySelector("#segunda").classList.remove("esconde")
     } else {
